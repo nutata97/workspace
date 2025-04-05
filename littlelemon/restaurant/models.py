efe4b9bd1341ec5f300fadb396b41bf137008c66
nutata97 <tantai97.working@gmail.com>
2025-04-05 11:43:02 +0700

    add migrations

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -10,11 +10,11 @@ class Booking(models.Model):
     def __str__(self):
         return f"{self.name} - {self.booking_date}"
     
-    class Menu(models.Model):
-        id = models.AutoField(primary_key=True)  # ID field as primary key
-        title = models.CharField(max_length=255)  # Title field with max length 255
-        price = models.DecimalField(max_digits=10, decimal_places=2)  # Price field with 2 decimal places
-        inventory = models.IntegerField()  # Inventory field
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True)  # ID field as primary key
+    title = models.CharField(max_length=255)  # Title field with max length 255
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # Price field with 2 decimal places
+    inventory = models.IntegerField()  # Inventory field
 
-        def __str__(self):
-            return f"{self.title} - ${self.price}"
+    def __str__(self):
+        return f"{self.title} - ${self.price}"
